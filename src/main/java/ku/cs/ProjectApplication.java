ebfa0cd8ffd10ba0c5d8d9c091de84ee0f6276c5
TanapatButsai <tanapat.bu@ku.th>
2022-08-05 23:29:15 +0700

    main

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -13,7 +13,7 @@ public class ProjectApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "Student ID", 800, 600);
         configRoute();
-        FXRouter.goTo("project");
+        FXRouter.goTo("login");
     }
 
     private static void configRoute() {
--- src/main/java/ku/cs/application/controllers/LoginController.java
+++ src/main/java/ku/cs/application/controllers/LoginController.java
@@ -1,4 +1,18 @@
 package ku.cs.application.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class LoginController {
+    String url = getClass().getResource("/ku/cs/login_images/ku_view.jpg").toExternalForm();
+    @FXML
+    private ImageView image_view_login;
+    @FXML
+    public void initialize() {
+        image_view_login.setImage(new Image(url));
+    }
+
+
+
 }
