30fce7e20558e29b880fbc5fbeb8552bbef2c1d0
Napakamol <98306196+Napakamol@users.noreply.github.com>
2022-09-06 11:23:38 +0700

    complaintpage

--- src/main/java/ku/cs/application/controllers/HomeController.java
+++ src/main/java/ku/cs/application/controllers/HomeController.java
@@ -2,7 +2,7 @@ package ku.cs.application.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import com.github.saacsos.FXRouter;
 import java.io.IOException;
 
 public class HomeController {
@@ -12,6 +12,16 @@ public class HomeController {
             com.github.saacsos.FXRouter.goTo("login");
         } catch (IOException e) {
             System.err.println("ไปที่หน้า login ไม่ได้");
+            System.err.println("ให้ตรวจสอบการกำหนด route");
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void handleNormalComplaint(ActionEvent actionEvent){
+        try {
+            com.github.saacsos.FXRouter.goTo("normalcomplaint");
+        } catch (IOException e) {
+            System.err.println("ไปที่หน้า ร้องเรียนทั่วไป ไม่ได้");
             System.err.println("ให้ตรวจสอบการกำหนด route");
             e.printStackTrace();
         }
