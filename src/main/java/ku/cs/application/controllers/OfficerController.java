837ee6b8ebe046730195ef675e5165667674af94
unknown <thanaphat.tan@ku.th>
2022-09-26 13:49:24 +0700

    add status report

--- src/main/java/ku/cs/application/controllers/OfficerController.java
+++ src/main/java/ku/cs/application/controllers/OfficerController.java
@@ -1,13 +1,16 @@
 package ku.cs.application.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import ku.cs.application.models.Officer;
 import com.github.saacsos.FXRouter;
+import ku.cs.application.models.OfficerID;
 import ku.cs.application.models.OfficerList;
-import ku.cs.application.services.ComplaintListDataSource;
 import ku.cs.application.services.DataSource;
 import ku.cs.application.services.OfficersListDataSource;
 
@@ -16,26 +19,85 @@ import java.io.IOException;
 
 public class OfficerController {
     private Officer officer;
+    private OfficerID officerID;
     private OfficerList officerList;
     private DataSource<OfficerList> dataSource;
     @FXML private Label topicLabel;
     @FXML private Label officerLabel;
+    @FXML private Label statusLabel;
     @FXML private ListView complaintListView;
+    @FXML private TextArea bodyTextArea;
+
 
     @FXML
     public void initialize(){
+        officerID = (OfficerID)FXRouter.getData();
+        if (officerID.getRole().equals("officer1")) {
+            System.out.println("User is Officer1");
+        }else if (officerID.getRole().equals("officer2")) {
+            System.out.println("User is Officer2");
+        }else if (officerID.getRole().equals("officer3")) {
+            System.out.println("User is Officer3");
+        }else if (officerID.getRole().equals("officer4")) {
+            System.out.println("User is Officer4");
+        }
         System.out.println("initialize MemberCardDetailController");
         dataSource = new OfficersListDataSource("data","officer.csv");
         officerList = dataSource.readData();
+        topicLabel.setText("");
+        statusLabel.setText("");
         showOfficerData();
+        handleSelectedListView();
     }
     public void showOfficerData(){
-        Officer officer1 = new Officer("benny","officer1","trasgh","wwwwwwwwww");
-        topicLabel.setText("");
         complaintListView.getItems().addAll(officerList.getAllOfficer());
-        //officerLabel.setText(officerList.setRole(officer));
-        officerLabel.setText(officer1.getRole());
+        complaintListView.refresh();
+        officerLabel.setText(officerID.setRole());
     }
+    private void handleSelectedListView(){
+        complaintListView.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<Officer>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Officer> observable,
+                                        Officer oldValue, Officer newValue) {
+                        System.out.println(newValue + " is selected");
+                        showSelectedOfficerData(newValue);
+                        setSelectedOfficerData(newValue);
+                    }
+                });
+
+    }
+    private void showSelectedOfficerData(Officer data){
+        statusLabel.setText(data.getStatus());
+        topicLabel.setText(data.getTopic());
+        bodyTextArea.setText(data.getBody());
+        bodyTextArea.setEditable(false);
+    }
+    private void  setSelectedOfficerData(Officer data){
+        officer = data;
+    }
+    @FXML private void handleDoneButton(ActionEvent actionEvent){
+        if (officer != null){
+            officer.setDone();
+            statusLabel.setText(officer.getStatus());
+            dataSource.writeData(officerList);
+        }
+    }
+    @FXML private void handleInProgress(ActionEvent actionEvent){
+        if (officer != null){
+            officer.setInProgress();
+            statusLabel.setText(officer.getStatus());
+            dataSource.writeData(officerList);
+        }
+    }
+    @FXML private void handleUnmanagedButton(ActionEvent actionEvent){
+        if (officer != null){
+            officer.setUnmanaged();
+            statusLabel.setText(officer.getStatus());
+            dataSource.writeData(officerList);
+        }
+    }
+
     @FXML
     public void handleBackButton(ActionEvent actionEvent) {
         try {
@@ -45,4 +107,5 @@ public class OfficerController {
             System.err.println("ให้ตรวจสอบการกำหนด route");
         }
     }
+
 }
--- src/main/java/ku/cs/application/models/Officer.java
+++ src/main/java/ku/cs/application/models/Officer.java
@@ -4,32 +4,75 @@ public class Officer extends Complaint{
     private String officerName;
     private String role;
     private String topic;
+    private boolean done;
+    private boolean inProgress;
+    private boolean unmanaged;
 
-    public Officer(String name,String CATEGORY,String headComplaint, String bodyComplaint) {
+    public Officer(String name,String CATEGORY,String headComplaint, String bodyComplaint,boolean done, boolean inProgress, boolean unmanaged) {
         super(headComplaint, bodyComplaint, CATEGORY);
         this.role = CATEGORY;
         this.topic = headComplaint;
         this.officerName = name;
+        this.done = done;
+        this.inProgress = inProgress;
+        this.unmanaged = unmanaged;
+    }
+    //setProcess
+    public boolean setDone() {
+        done = true;
+        inProgress = false;
+        unmanaged = false;
+        return done;
+    }
+    public boolean setInProgress() {
+        inProgress = true;
+        done = false;
+        unmanaged =false;
+        return inProgress;
+    }
+    public boolean setUnmanaged() {
+        unmanaged = true;
+        done = false;
+        inProgress =false;
+        return unmanaged;
+    }
+    public boolean isDone() {
+        return done;
+    }
+    public boolean isInProgress() {
+        return inProgress;
+    }
+    public boolean isUnmanaged() {
+        return unmanaged;
     }
 
     public String getName() {
         return officerName;
     }
-
     public String getTopic() {
         return topic;
     }
-
+    public  String getBody(){
+        return getBodyComplaint();
+    }
     public String getRole() {
-        if (role.equals("officer1")){
-            return "เรื่องร้องเรียงทั่วไป";
-        } else if (role.equals("officer2")) {
-            return "เรื่องร้องเรียงอาจารย์/บุคลาการ";
-        } else if (role.equals("officer3")){
-            return "เรื่องร้องเรียงอาคาร แลพสถานที่";
-        } else if (role.equals("officer4")) {
-            return  "เรื่องร้องเรียงการลงทะเบียนเรียน";
+        return role;
+    }
+    public String getStatus(){
+        if (isDone()){
+            System.out.println("111");
+            return "ดำเนินการแล้ว";
+        }else if (isInProgress() == true){
+            System.out.println("11");
+            return "อยู่ระหว่างการดำเนินการแล้ว";
+        }else if (isUnmanaged() == true){
+            System.out.println("1");
+            return "ยังไม่ถูกจัดการ";
         }
         return "";
+    }
+    @Override
+    public String toString() {
+        return topic;
     }
 }
