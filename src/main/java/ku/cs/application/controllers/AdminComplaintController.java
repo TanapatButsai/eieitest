3bfb339feeb30e82355acd486d129a9b620d53e7
Peerapattuajing <peerapat.pak@ku.th>
2022-09-26 16:13:08 +0700

    add admin complaint scene

---
+++ src/main/java/ku/cs/application/controllers/AdminComplaintController.java
@@ -0,0 +1,21 @@
+package ku.cs.application.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import ku.cs.application.models.Complaint;
+
+public class AdminComplaintController {
+    @FXML private ListView<Complaint> complaintListView;
+    @FXML private Label reporterLabel;
+    @FXML private Label reporttypeLabel;
+    @FXML private Label titleLabel;
+    @FXML private TextArea descriptionLabel;
+
+
+
+
+
+}
